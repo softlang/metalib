f68ff96572e610df8f17b9e06547214ab13ba2c2
Simon Schauß <simon.schauss@gmail.com>
2017-11-26 18:26:53 +0100

    fix #44

--- tools/metadocs/src/cljs/metadocs/wrapper/highlight.cljs
+++ tools/metadocs/src/cljs/metadocs/wrapper/highlight.cljs
@@ -2,7 +2,10 @@
   (:require [cljsjs.highlight]
             [cljsjs.highlight.langs.java]
             [cljsjs.highlight.langs.haskell]
+            [cljsjs.highlight.langs.python]
             [cljsjs.highlight.langs.scala]
+            [cljsjs.highlight.langs.scheme]
+            [cljsjs.highlight.langs.xml]
             [reagent.core :as reagent]))
 
 (defn wrapper [block]
